da93e99a0bb465cd99f44edefe5bc2c6441522dd
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-11-14 09:03:52 +0000

    reformat into package

--- splink_data_generation/estimate_splink.py
+++ splink_data_generation/estimate_splink.py
@@ -4,29 +4,19 @@ from pandas import DataFrame
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 
-from pyspark.sql import Row
-
 from splink.iterate import iterate
 
-import logging
-
-logging.basicConfig()  # Means logs will print in Jupyter Lab
-
-# Set to DEBUG if you want splink to log the SQL statements it's executing under the hood
-logging.getLogger("splink").setLevel(logging.INFO)
 from splink import Splink
 
 
-def estimate(df_gammas: DataFrame, settings: dict):
+def estimate(df_gammas: DataFrame, settings: dict, spark: SparkSession):
     """Take pandas datafrae of gammas and estimate splink model
 
     Args:
         df_gammas (DataFrame): df_gammas
         settings (dict): Splink settings dictionary
+        spark (SparkSession): SparkSession object
     """
-
-    sc = SparkContext.getOrCreate()
-    spark = SparkSession(sc)
 
     df = spark.createDataFrame(df_gammas)
 
